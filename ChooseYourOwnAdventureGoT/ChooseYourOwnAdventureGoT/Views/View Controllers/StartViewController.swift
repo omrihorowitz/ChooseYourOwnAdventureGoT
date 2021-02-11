2065bc75f2f3486f66362e3194f8d797b958345a
Omri Horowitz <horowitzomri@gmail.com>
2021-02-11 11:07:24 -0800

    commit

--- ChooseYourOwnAdventureGoT/ChooseYourOwnAdventureGoT/Views/View Controllers/StartViewController.swift
+++ ChooseYourOwnAdventureGoT/ChooseYourOwnAdventureGoT/Views/View Controllers/StartViewController.swift
@@ -25,7 +25,7 @@ class StartViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toStoryVC" {
-
+            
         }
 
     }
